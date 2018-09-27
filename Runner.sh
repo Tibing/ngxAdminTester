8b2173dfa3e9f57fde692474fd337a6dc912c521
Valeria <sholomova96@gmail.com>
2018-09-27 23:08:46 +0300

    Script Runner.sh for tests run is added

---
+++ Runner.sh
@@ -0,0 +1,14 @@
+#$1 - browser
+CONF="test.conf.js"
+echo "Put browser into configuration file"
+if [ "$1" == "chrome" ]; then 
+cat $CONF | sed -e "s|<BROWSER>|$1|g" | sed -e "s|<chrome>|$1|g"  > config.js 
+else 
+cat $CONF | sed -e "s|<BROWSER>|$1|g"  > config.js 
+fi
+#workaround to avoid protractor(or selenium bug)
+#when firefox target is defined
+#and tests run in Chrome instead of Firefox
+webdriver-manager update
+webdriver-manager start
+protractor config.js > log.txt
